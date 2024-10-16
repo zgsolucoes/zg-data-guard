f8f6d8a7b88a4214a40c89013bc546dcdab14f26
Luiz Henrique <luizhenrique@zgsolucoes.com.br>
2024-10-16 16:33:15 -0300

    feat: add SQL queries files for database and database instance storages

---
+++ internal/database/sqls/select_database_instances.sql
@@ -0,0 +1,35 @@
+SELECT di.id,
+	   di.name,
+	   hci.host,
+	   hci.port,
+	   hci.host_connection,
+	   hci.port_connection,
+	   hci.admin_username,
+	   hci.admin_password,
+	   di.ecosystem_id,
+	   e.display_name,
+	   di.database_technology_id,
+	   dt.name,
+	   dt.version,
+	   di.enabled,
+	   di.roles_created,
+	   di.note,
+	   di.created_at,
+	   di.created_by_user_id,
+	   au.name,
+	   di.updated_at,
+	   di.disabled_at,
+	   di.last_database_sync,
+	   di.connection_status,
+	   di.last_connection_test,
+	   di.last_connection_result
+FROM database_instances di
+	 JOIN host_connection_info hci
+		ON di.host_connection_info_id = hci.id
+	 JOIN application_users au
+		ON di.created_by_user_id = au.id
+	 JOIN database_technologies dt
+		ON di.database_technology_id = dt.id
+	 JOIN ecosystems e
+		ON di.ecosystem_id = e.id
+WHERE 1 = 1
---
+++ internal/database/sqls/select_databases.sql
@@ -0,0 +1,29 @@
+SELECT db.id,
+	   db.name,
+	   db.current_size,
+	   di.id,
+	   di.name,
+	   e.id,
+	   e.display_name,
+	   dt.id,
+	   dt.name,
+	   dt.version,
+	   db.enabled,
+	   db.roles_configured,
+	   db.description,
+	   db.created_by_user_id,
+	   au.name,
+	   db.created_at,
+	   db.updated_at,
+	   di.last_database_sync,
+	   db.disabled_at
+FROM databases db
+	 JOIN database_instances di
+		ON db.database_instance_id = di.id
+	 JOIN application_users au
+		ON db.created_by_user_id = au.id
+	 JOIN database_technologies dt
+		ON di.database_technology_id = dt.id
+	 JOIN ecosystems e
+		ON di.ecosystem_id = e.id
+WHERE 1 = 1
