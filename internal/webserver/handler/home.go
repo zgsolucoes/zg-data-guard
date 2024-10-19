b07fb127663cbdce6563ecb60fd10677ecc90be7
Luiz Henrique <luizhenrique@zgsolucoes.com.br>
2024-10-19 00:15:44 -0300

    feat: add two buttons to the home page for internal login and open Swagger UI

--- internal/webserver/handler/home.go
+++ internal/webserver/handler/home.go
@@ -8,18 +8,22 @@ import (
 )
 
 type HomeData struct {
-	LoginURL     string
-	AppVersion   string
-	AppBuildTime string
+	LoginURL             string
+	SwaggerURL           string
+	LoginInternalUserURL string
+	AppVersion           string
+	AppBuildTime         string
 }
 
 func HomeHandler(w http.ResponseWriter, _ *http.Request) {
 	t, _ := template.ParseFiles("internal/webserver/templates/home.html")
 
 	data := HomeData{
-		LoginURL:     config.GetApplicationURL(),
-		AppVersion:   config.GetBuildInfo().Version,
-		AppBuildTime: config.GetBuildInfo().BuildTime,
+		SwaggerURL:           "docs/index.html",
+		LoginInternalUserURL: "auth/internal",
+		LoginURL:             config.GetApplicationURL(),
+		AppVersion:           config.GetBuildInfo().Version,
+		AppBuildTime:         config.GetBuildInfo().BuildTime,
 	}
 	_ = t.Execute(w, data)
 }
